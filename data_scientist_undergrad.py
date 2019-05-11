f9996bd8432426c4d6176474fa537fbd5d630527
TSTEP99 <43427263+TSTEP99@users.noreply.github.com>
2019-05-10 19:05:55 -0500

    Delete data_scientist_undergrad.py

--- data_scientist_undergrad.py
+++
@@ -1,7 +0,0 @@
-import csv;
-import pandas as pd;
-import numpy as np;
-
-df=pd.read_csv("survey_results_public.csv");
-df=df[["DevType","UndergradMajor"]]; #limits down to 
-df=df[ df.DevType==df.DevType]; #gets rid of all data with non existent values
